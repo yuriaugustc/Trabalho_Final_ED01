4361c55942a14a57a0b7bb158308ab6c470f4858
Yuri Augusto da Costa <yuri.costa@sedoc.local>
2021-10-21 14:24:52 -0300

    Etapa 1 ok

--- imm.c
+++ imm.c
@@ -1,30 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "imm.h"
 
 int main(int argc, char *argv[]){
 
     if(strcmp(argv[1], "-open") == 0){
-        printf("Okay, -open abrindo ;w;\n");
+        imm_open_file();
         return 0;
     }
     if(strcmp(argv[1], "-convert") == 0){
-        printf("Okay, -convert convertendo ;w;\n");
+        imm_convert();
         return 0;
     }
     if(strcmp(argv[1], "-segment") == 0){
-        printf("Okay, -segment segmentando ;w;\n");
+        imm_segment();
         return 0;
     }
     if(strcmp(argv[1], "-cc") == 0){
-        printf("Okay, -cc cczando ;w;\n");
+        imm_cc();
         return 0;
     }
     if(strcmp(argv[1], "-lab") == 0){
-        printf("Okay, -lab labindo ;w;\n");
+        imm_lab();
         return 0;
     }
-    else{
-        printf("the command '%s' doesn't part of imm's mount command. try 'imm -help' to find some usable command.\n", argv[1]);
+    if(strcmp(argv[1], "-help") == 0){
+        printf("\t\t\t\t---- imm list commands -----\n\n");
+        // -open;
+        printf("imm -open : \t\t\t\t\t\t");
+        printf("Open an image (text format or binary format) and show the pixels value on the screen.\n\n");
+        // -convert;
+        printf("imm -convert [file.txt] [file.imm] : \t\t\t");
+        printf("Convert an image on file.txt format to file.imm format.\n\n");
+        // -segment;
+        printf("imm -segment [thr] [file.imm] [segfile.imm] : \t\t");
+        printf("Does a thresholding with a image thr value of file.mm.\n\t\t\t\t\t\t\t");
+        printf("That can be used with .txt files also:\n \t\t\t\t\t\t\t\t");
+        printf("\"imm -segment thr arq1.txt arq2.txt\"\n\t\t\t\t\t\t\t");
+        printf("That can be used with .txt/.imm mixed files:\n \t\t\t\t\t\t\t\t");
+        printf("\"imm -segment thr arq1.txt arq2.imm\" \n \t\t\t\t\t\t\t\t");
+        printf("\"imm -segment thr arq1.imm arq2.txt\"\n\n");
+        // -cc;
+        printf("imm -cc [segfile.imm] [outfile.imm]: \t\t\t");
+        printf("Detects an image connected components.\n\t\t\t\t\t\t\t");
+        printf("That can be used with .txt and .imm files.\n\n");
+        // -lab;
+        printf("imm -lab [imlab.txt] [imlabout.txt]: \t\t\t");
+        printf("Shows a path to go through in a labirint.\n\t\t\t\t\t\t\t");
+        printf("That can be used with .txt and .imm files.\n\n");
     }
+    else{
+        printf("the command '%s' doesn't part of imm command list. try 'imm -help' to find some usable command.\n", argv[1]);
+    }
+    return 0;
 }
---
+++ imm.h
@@ -0,0 +1,17 @@
+#ifndef _immh_
+#define _immh_
+
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
+#define EMPTY_LIST -5
+
+int imm_open_file();
+int imm_convert();
+int imm_segment();
+int imm_cc();
+int imm_lab();
+
+#endif
